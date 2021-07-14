c20da0a75770ff0f29ad6339f39598593c4b1adb
Xinying-Zheng <60825536+Xinying-Zheng@users.noreply.github.com>
2021-07-14 14:14:28 +0800

    Update filtered_Gaussian.m

--- Project03_Rayleigh fading/filtered_Gaussian.m
+++ Project03_Rayleigh fading/filtered_Gaussian.m
@@ -8,11 +8,15 @@ w_q=sigma*randn(1,N);
 g_i(1)=0;
 g_q(1)=0;
 for i=1:N
+    % shift (different path) and add the guassian noise
     g_i(i+1)=zeta*g_i(i)+(1-zeta)*w_i(1,i);
     g_q(i+1)=zeta*g_q(i)+(1-zeta)*w_q(1,i);
 end
+% generate entire signal
 r_tilde=g_i+j*g_q;
+% autocorr of simulate
 Rx=autocorr(r_tilde(2:N+1),10/fmT);
+% autocorr of theory
 for fm_tau=0:fmT:10
     delta=round(fm_tau/fmT);
     Rx_theory(delta+1)=(sigma^2*(1-zeta)/(1+zeta)).*zeta.^(fm_tau/fmT);
