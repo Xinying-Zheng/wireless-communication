c3e1672e67f9ee23a79e8947249295ea64a77342
Xinying-Zheng <60825536+Xinying-Zheng@users.noreply.github.com>
2022-03-09 09:19:36 +0800

    Update Erlang_B.m

--- Project01_Erlang_B/Erlang_B.m
+++ Project01_Erlang_B/Erlang_B.m
@@ -3,7 +3,7 @@
 % - B(rho, m)= (rho^m)/(m!*\sum^m_{k=0}(rho^k/k!))
 % if we compute B by above equation, the result will overflow in large m
 % the approach is change the equation into recursive form (ref. folk theorem)
-% B_{m+1}=(B_m*(rho/m))/(1+B_m*(rho/m)) , B_0=1
+% B_{m+1}=(B_m*(rho/m))/(1+B_m*(rho/m)) , B_0=1......(1)
 
 function [ B ] = Erlang_B( m,rho )
     B=1;
@@ -12,5 +12,8 @@ function [ B ] = Erlang_B( m,rho )
     end
 end
 
+% If we want to driver the recursive form. we can first write the equation of B{m+1} and B{m}
+% Then we subtute the B{m} into numberator of B{m+1} the we can compute the numberator and denominator.
+% and we can obtain the equation (1).
 
 
